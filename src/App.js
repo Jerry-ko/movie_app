9aff434fd22611a61c2cdac7877055b0c2404296
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-08-17 10:35:40 +0900

    #2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import Potato from './Potato'
+
 function App() {
-  return <div><h1>Hello</h1></div>;
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+  );
 }
+
 
 export default App;
