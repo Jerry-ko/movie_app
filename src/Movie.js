0b44bfa8d0aae7a33b7d5012b195e4f539d6cedd
Minju Ko <70134713+Jerry-ko@users.noreply.github.com>
2021-08-19 10:51:02 +0900

    #4.5 Cutting the summary

--- src/Movie.js
+++ src/Movie.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import "./Movie.css"
 
 function Movie({ year, title, summary, poster, genres }) {
-    return <div className="movies__movie">
+    return <div className="movie">
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
             <h3 className="movie__title">{title}</h3>
@@ -12,7 +12,7 @@ function Movie({ year, title, summary, poster, genres }) {
                 {genres.map((genre, index) => (
                     <li key={index} className="genres__genre">{genre}</li>
                 ))}</ul>
-            <p className="movie__summary">{summary}</p>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
     </div>
 }
